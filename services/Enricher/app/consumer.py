4964bb98861c8db2766f4546c0671a14638a891f
avi <a0548589558@gmail.com>
2025-08-27 15:02:12 +0300

    Enricher is able to receive events and send them forward

---
+++ services/Enricher/app/consumer.py
@@ -0,0 +1,15 @@
+from kafka import KafkaConsumer
+import json
+import os
+
+
+class Consumer:
+    @staticmethod
+    def get_consumer_events(topic):
+        """return an object with all the last events waiting in the kafka server for the specified topic"""
+        bootstrap_server = os.getenv("KAFKA_BOOTSTRAP", "localhost:9092")
+        return KafkaConsumer(topic,
+                                         group_id='my-group',
+                                         value_deserializer=lambda m: json.loads(m.decode('ascii')),
+                                         bootstrap_servers=[bootstrap_server]
+                                      )
---
+++ services/Enricher/app/producer.py
@@ -0,0 +1,22 @@
+from kafka import KafkaProducer
+import json
+import os
+
+
+class Producer:
+    def __init__(self):
+        """initialize with a KafkaProducer object"""
+        bootstrap_server = os.getenv("KAFKA_BOOTSTRAP", "localhost:9092")
+        self.__producer = KafkaProducer(bootstrap_servers=[bootstrap_server],
+                                         value_serializer=lambda x:
+                                         json.dumps(x).encode('utf-8')
+                                        )
+
+    def publish_event(self, topic, event) -> None:
+        """
+        publish an event to a specific topic on the liked kafka server
+        :param event: the event to upload
+        :param topic: the specified topic
+        """
+        self.__producer.send(topic, event)
+        self.__producer.flush()
